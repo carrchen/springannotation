67b19d61fbc762c65883f29fb047c3d939cfd778
lengfeng <lengfeng@suneee.com>
2018-12-12 16:28:20 +0800

    修改xml

--- src/main/java/com/chencai/io/bio/BIOClient.java
+++ src/main/java/com/chencai/io/bio/BIOClient.java
@@ -1,4 +1,23 @@
 package com.chencai.io.bio;
 
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.UUID;
+
 public class BIOClient {
+    public static void main(String[] args) {
+        try {
+            Socket client = new Socket("localhost", 8080);
+            OutputStream os = client.getOutputStream();
+            String name = UUID.randomUUID().toString();
+            os.write(name.getBytes());
+            os.close();
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/com/chencai/io/bio/BIOServer.java
+++ src/main/java/com/chencai/io/bio/BIOServer.java
@@ -1,4 +1,41 @@
 package com.chencai.io.bio;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class BIOServer {
+
+    ServerSocket server;
+
+    public BIOServer(int port) {
+        try {
+            server = new ServerSocket(port);
+            System.out.println("服务端已启动，监听端口为：8080");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listener() throws IOException {
+        while (true) {
+            Socket client = server.accept();
+            InputStream is = client.getInputStream();
+            byte[] buff = new byte[1024];
+            int len = is.read(buff);
+            if (len > 0) {
+                String msg = new String(buff, 0, len);
+                System.out.println("收到：" + msg);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            new BIOServer(8080).listener();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
